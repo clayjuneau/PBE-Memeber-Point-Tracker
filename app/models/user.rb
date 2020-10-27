945dad560ad7041e08cfa0f8489e346b11d37b74
Carter Mitchell <cjmitchell333@gmail.com>
2020-10-27 18:49:29 -0500

    Revert "Merge branch 'cmitchell/story/47-Role-Edit' into master"
    This reverts commit 60e43df67d28a5baf9953fca9d290c291164d018, reversing
    changes made to fa761546003afbc9d23062d5580ea431a3d828ed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
 class User < ApplicationRecord
     has_secure_password
-
-    def edit
-        @user = User.find(params[:firstname, :lastname])
-    end
-
-    def update
-        @user = User.find(params[:firstname, :lastname])
-    end
 end
