8c7a42d3732fdfd83ad6a7cd12d11728aa1289e5
Riley Norris <rileydnorris@gmail.com>
2020-10-28 09:14:57 -0500

    Update method of retrieving events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,24 +6,6 @@ class HomeController < ApplicationController
             @user = User.find(params[:id])
         end
 
-        # Find the other team's user object by seeing which user has the same email as our user
-        @events = Customer.includes(:events).where(:email => current_user.email)
-        puts @events
-        if @events == nil
-            @events = []
-        end
-        
-        # event_ids = []
-        # if customer != nil
-        #     # Look up user_id in attendance table
-        #     event_ids = Attendance.where(user_id: customer.id).pluck(:event_id)
-        # end
-        
-        # if event_ids != []
-        #     # Look up event_id in event table
-        #     @events = Event.where(id: event_ids).order('date ASC')
-        # else
-        #     @events = []
-        # end
+        @events = Event.includes(:customers).where(customers: { email: current_user.email })
     end
 end
