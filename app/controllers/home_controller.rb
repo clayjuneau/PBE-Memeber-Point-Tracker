e3fd4866bea8b547d0f55a55b3fbab3d444df2d8
Riley Norris <rileydnorris@gmail.com>
2020-11-10 00:51:06 -0600

    Remove error when no events are present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+
+    helper_method :point_percentage
     
     # Display home page
     def index
@@ -9,5 +11,16 @@ class HomeController < ApplicationController
         if @user == nil
             @user = current_user
         end
+    end
+
+    def point_percentage(user_id)
+        user_event_count = user_events(user_id).count.to_f
+        mandatory_event_count = mandatory_events.count.to_f
+
+        if user_event_count == 0 || mandatory_event_count == 0
+            return 0
+        end
+
+        return user_event_count / mandatory_event_count
     end
 end
